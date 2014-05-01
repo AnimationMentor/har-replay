832a3238f904b4de5e9c04ae8998653d580e37d7
Mike Mahony <mikem@animationmentor.com>
2014-05-01 11:17:22 -0700

    Don't worry about any bad https certs. Log errors, should a request fail. Tabs to spaces. Also, site filter works with https prototcols.

--- main.js
+++ main.js
@@ -11,54 +11,57 @@ var moment = require("moment");
 var commander = require("commander");
 
 commander
-	.option('-s, --site <site>', 'only fire requests that are for this domain (ignore everything else)')
-	.option('-f, --file <file>', 'HAR file to replay')
-	.parse(process.argv);
+    .option('-s, --site <site>', 'only fire requests that are for this domain (ignore everything else)')
+    .option('-f, --file <file>', 'HAR file to replay')
+    .parse(process.argv);
 
 // Check arguments (file is required)
 if (commander.file == undefined) {
-	commander.outputHelp();
-	process.exit();
+    commander.outputHelp();
+    process.exit();
 }
+
+// Don't worry about any bad https certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 // Do the Bartman! :)
 fs.readFile(commander.file, function(err, data) {
-	if (err) throw err;
-	var har = JSON.parse(data);
+    if (err) throw err;
 
-	var basetime = moment(har['log']['entries'][0]['startedDateTime']);
+    var har = JSON.parse(data),
+        basetime = moment(har['log']['entries'][0]['startedDateTime']);
 
-	// Passing in an object
-	_.forEach(har['log']['entries'], function(entry) {
+    // Passing in an object
+    _.forEach(har['log']['entries'], function(entry) {
 
-		// Ignore other sites if so requested
-		if (commander.site != undefined && entry.request.url.substring(7, 7 + commander.site.length) != commander.site)
-			return;
+        // Ignore other sites if so requested
+        if (commander.site != undefined && entry.request.url.split('://')[1].split('/')[0] != commander.site)
+            return;
 
-		// How late did this request happen?
-		var diff = moment(entry['startedDateTime']).diff(basetime, 'miliseconds');
+        // How late did this request happen?
+        var diff = moment(entry['startedDateTime']).diff(basetime, 'miliseconds');
 
-		// Send a request into the future
-		_.delay(function() {
-			// New request
-			var req = request({
-				url: entry.request.url,
-				method: entry.request.method,
-				// reformat headers from HAR format to a dict
-				headers: _.reduce(entry.request.headers, function(memo, e) {
-					memo[e['name']] = e['value'];
-					return memo;
-				}, {})
-			}, function(error, response, body) {
-				// Just print a status, drop the files as soon as possible
-				console.log(entry.request.url + " => " + response.statusCode);
-			});
+        // Send a request into the future
+        _.delay(function() {
+            // New request
+            var req = request({
+                url: entry.request.url,
+                method: entry.request.method,
+                // reformat headers from HAR format to a dict
+                headers: _.reduce(entry.request.headers, function(memo, e) {
+                    memo[e['name']] = e['value'];
+                    return memo;
+                }, {})
+            }, function(error, response, body) {
+                // Just print a status, drop the files as soon as possible
+                console.log(entry.request.url + " " + entry.request.method + (response ? " => " + response.statusCode: " X> " + error + ':' + body));
+            });
 
-			// Garbage collect, if we can (if started with --expose-gc)
-			if (global.gc) {
-				global.gc();
-			}
-		}, diff);
-	});
+            // Garbage collect, if we can (if started with --expose-gc)
+            if (global.gc) {
+                global.gc();
+            }
+        }, diff);
+    });
 });
 
